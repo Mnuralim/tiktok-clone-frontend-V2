3afd6e4a1979d53da2756c025885e2e7eb401115
mnuralim <shadownur345.com>
2024-06-07 18:55:39 +0800

    add callback url

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import ButtonLoginGoogle from './components/button-login-google'
 
+export const dynamic = 'force-dynamic'
+
 const Page = () => {
   return (
     <section className="h-[100dvh] flex items-center justify-center bg-white">
-      <div className="flex flex-col items-center justify-between h-full px-4 py-4 w-full lg:w-2/3">
+      <div className="flex flex-col items-center justify-between w-full h-full px-4 py-4 lg:w-2/3">
         <div className="">
-          <div className="text-center px-3">
+          <div className="px-3 text-center">
             <h1 className="text-2xl font-bold">Sign up for TikTok</h1>
             <p className="text-[rgba(22,24,35,0.5)]">
               Create a profile, follow other accounts, make your own videos, and more.
             </p>
           </div>
         </div>
-        <div className="flex flex-col gap-3 px-3 w-full">
+        <div className="flex flex-col w-full gap-3 px-3">
           <ButtonLoginGoogle />
-          {/* <ButtonLoginGithub /> */}
         </div>
         <div>
           <p className="text-[rgba(22,24,35,0.5)] text-xs text-center">
