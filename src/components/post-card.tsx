8c58e830501a5464649059267711b998d410654a
mnuralim <shadownur345.com>
2024-06-07 16:23:39 +0800

    add buffering ,arker

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -3,6 +3,7 @@ import PostAction from '@/components/post-action'
 import React, { useEffect, useRef, useState } from 'react'
 import PostCaption from './post-caption'
 import { FaPlay } from 'react-icons/fa'
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
 interface Props {
   post: IPost
@@ -12,6 +13,38 @@ const PostCard = ({ post }: Props) => {
   const [isPlaying, setIsPlaying] = useState<boolean>(false)
   const vidRef = useRef<HTMLVideoElement>(null)
   const videoContainerRef = useRef<HTMLDivElement>(null)
+  const [isBuffering, setIsBuffering] = useState<boolean>(false)
+
+  useEffect(() => {
+    const handleWaiting = () => {
+      setIsBuffering(true)
+    }
+
+    const handlePlaying = () => {
+      setIsBuffering(false)
+      setIsPlaying(true)
+    }
+
+    const handleCanPlay = () => {
+      setIsBuffering(false)
+    }
+
+    const currentVideoRef = vidRef.current
+
+    if (currentVideoRef) {
+      currentVideoRef.addEventListener('waiting', handleWaiting)
+      currentVideoRef.addEventListener('playing', handlePlaying)
+      currentVideoRef.addEventListener('canplay', handleCanPlay)
+    }
+
+    return () => {
+      if (currentVideoRef) {
+        currentVideoRef.removeEventListener('waiting', handleWaiting)
+        currentVideoRef.removeEventListener('playing', handlePlaying)
+        currentVideoRef.removeEventListener('canplay', handleCanPlay)
+      }
+    }
+  }, [])
 
   const handleVideoClick = () => {
     if (vidRef.current) {
@@ -64,7 +97,7 @@ const PostCard = ({ post }: Props) => {
   }, [])
 
   return (
-    <div ref={videoContainerRef} className="h-full w-full relative flex items-center  flex-col">
+    <div ref={videoContainerRef} className="relative flex flex-col items-center w-full h-full">
       <video
         src={post.videoUrl}
         ref={vidRef}
@@ -76,13 +109,18 @@ const PostCard = ({ post }: Props) => {
       <div className="absolute right-[10px] z-10 bottom-16">
         <PostAction isVideoPlaying={isPlaying} post={post} />
       </div>
-      <div className="absolute bottom-16 left-3 z-10">
+      <div className="absolute z-10 bottom-16 left-3">
         <PostCaption isVideoPlaying={isPlaying} post={post} />
       </div>
       <div className="gradient-overlay"></div>
       {isPlaying ? null : (
         <div className="absolute inset-1/2 transform -translate-x-[20px] -translate-y-[40px] pointer-events-none">
           <FaPlay size="50" color="white" />
+        </div>
+      )}
+      {!isBuffering ? null : (
+        <div className="absolute inset-1/2 transform -translate-x-[20px] -translate-y-[40px] pointer-events-none">
+          <AiOutlineLoading3Quarters size="50" color="white" className="animate-spin-slow" />
         </div>
       )}
     </div>
